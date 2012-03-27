54a87f281929189e6ac30918bb92f9050cfc02c3
Michael Specht <micha.specht@gmail.com>
2012-03-27 10:29:43 +0200

    no antialiasing, no QR code

--- update.py
+++ update.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from xml.dom.minidom import parse, parseString
 import os
 from subprocess import *
@@ -33,11 +34,11 @@ for item in dom.getElementsByTagName('item'):
     if not os.path.exists(pngPath):
         with open(htmlPath, 'w') as fout:
             fout.write("<html><head><link href='../styles.css' type='text/css' rel='stylesheet' media='all' /></head><body><div class='post'>\n")
-            fout.write("<img class='qr' src='http://qrcode.kaywa.com/img.php?s=12&d=http%3A%2F%2Fwww.faz.net%2F-gum-6y6jv' />\n")
+            #fout.write("<img class='qr' src='http://qrcode.kaywa.com/img.php?s=12&d=http%3A%2F%2Fwww.faz.net%2F-gum-6y6jv' />\n")
             fout.write("<h1>" + getText(item.getElementsByTagName('title')[0].childNodes).encode('utf-8') + "</h1>\n\n")
             fout.write(getText(item.getElementsByTagName('description')[0].childNodes).encode('utf-8') + "\n\n")
             fout.write("</div><center><img src='../hr.png' style='width: 30%;'/></center></body></html>\n")
         os.system("wkhtmltopdf --encoding utf8 --page-width 48 --page-height 3000 -B 0 -L 0 -T 0 -R 0 \"" + htmlPath + "\" \"" + pdfPath + "\"")
-        os.system("convert -density 219 \"" + pdfPath + "\" -trim -monochrome \"" + pngPath + "\"");
+        os.system("convert +antialias -density 219 \"" + pdfPath + "\" -trim -monochrome \"" + pngPath + "\"");
         os.system("rm \"" + htmlPath + "\"")
         os.system("rm \"" + pdfPath + "\"")
